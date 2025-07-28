ffefc67866bb574e0f7ec070a39e722c463f42bb
charangowdat <charantcharant252@gmail.com>
2025-07-28 23:35:27 +0530

    register user functionalities and home page setup

---
+++ accounts/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import User, UserProfile
+from django.contrib.auth.admin import UserAdmin
+# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+  list_display = ('email', 'first_name', 'last_name', 'username', 'role', 'is_active')
+  ordering = ('-date_joined',)
+  #idk, wrote this to make password not editable in admin panel
+  filter_horizontal = ()
+  list_filter = ()
+  fieldsets = ()
+
+#now it's visible in admin panel
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(UserProfile)
